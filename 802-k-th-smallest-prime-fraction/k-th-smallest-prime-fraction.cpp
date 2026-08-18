06654e85f99db34185f610c2d4d6e55f49c159cb
Pushkar Kumar <pushkarchauhan.kumar@gmail.com>
2026-08-18 20:17:21 +0530

    Time: 1299 ms (5.01%) | Memory: 119.4 MB (32.79%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -3,16 +3,30 @@ public:
     typedef pair<double,pair<int,int>> p;
     vector<int> kthSmallestPrimeFraction(vector<int>& arr,int k) {
         int n=arr.size();
-        priority_queue<p> pq;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                double fraction=(double)arr[i]/arr[j];
-                pq.push({fraction,{arr[i],arr[j]}});
-                if(pq.size()>k){
-                    pq.pop();
-                }
+        priority_queue<p,vector<p>,greater<p>> pq;
+        set<pair<int,int>> st;
+        pq.push({(double)arr[0]/arr[n-1],{0,n-1}});
+        st.insert({0,n-1});
+        int cnt=0;
+        while(!pq.empty()){
+            p curr=pq.top();
+            pq.pop();
+            double fraction=curr.first;
+            int i=curr.second.first;
+            int j=curr.second.second;
+            cnt++;
+            if(cnt==k){
+                return {arr[i],arr[j]};
+            }
+            if(i+1<j && st.find({i+1,j})==st.end()){
+                pq.push({(double)arr[i+1]/arr[j],{i+1,j}});
+                st.insert({i+1,j});
+            }
+            if(i<j-1 && st.find({i,j-1})==st.end()){
+                pq.push({(double)arr[i]/arr[j-1],{i,j-1}});
+                st.insert({i,j-1});
             }
         }
-        return {pq.top().second.first,pq.top().second.second};
+        return {};
     }
 };
